0b2ecd6f0526e7ba965c827ae3cb0adde7590742
VItchanJin <64739304+VItchanJin@users.noreply.github.com>
2021-10-27 13:48:29 +0900

    Add files via upload

--- coolsms_API.py
+++ coolsms_API.py
@@ -8,7 +8,7 @@ def sendMsg(deg):
     params['type'] = 'sms'
     params['to'] = '01086461870' #받는번호
     params['from'] = '01086461870' #보내는번호
-    params['text'] = '고온이 감지되었습니다! 단말기를 확인해주세요! 단말기온도: %.1f도'%(deg) #문자 내용
-    cool = Message(api_key=api_secret)
+    params['text'] = '배전반 A에서 고온이 감지되었습니다! 배전반 A를 확인해주세요! 배전반A 온도: %.1f도'%(deg) #문자 내용
+    cool = Message(api_key,api_secret)
     response =cool.send(params)
     return response            
